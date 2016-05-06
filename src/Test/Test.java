3a108a06a1bdb4597359219ce56e7b1f876ad635
sandaru <sandaru.14@cse.mrt.ac.lk>
2016-05-06 17:28:14 +0530

    test commit

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -3,18 +3,7 @@ public class Test {
     static int a=2;
     int b=4;
     public static void main(String args[]) throws InterruptedException{
-        Test t =new Test();
-        t.setName();
-        //System.out.println("staic:"+t.b);
-        //System.out.println("static:"+a);
-        //t.c();
-       // Thread p=new Thread(new A());
-       // p.start();
-        //p.join(1000);
-        //System.out.println("noo");
-       // A a=new A();
-        //a.start();
-       /// a.start();
+    
     }
     void  c(){
         System.out.println("instance method:"+a);
